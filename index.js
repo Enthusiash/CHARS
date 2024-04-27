5bbd3c810399774c238bb9accc814550c324dbdb
Syntaxsh <ashleyotchengco@gmail.com>
2024-04-27 15:56:33 +0800

    uncomment prod config about build in index.js

--- index.js
+++ index.js
@@ -40,13 +40,13 @@ app.use(recommenderRouter);
 
 app.use("/uploads", express.static("./uploads"));
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client/build")));
+// if (process.env.NODE_ENV === "production") {
+// 	app.use(express.static(path.join(__dirname, "client/build")));
 
-	app.get("*", function (req, res) {
-		res.sendFile(path.join(__dirname, "client/build", "index.html"));
-	});
-}
+// 	app.get("*", function (req, res) {
+// 		res.sendFile(path.join(__dirname, "client/build", "index.html"));
+// 	});
+// }
 
 app.listen(port, () => {
 	console.log(`Server is running at port: ${port}`);
